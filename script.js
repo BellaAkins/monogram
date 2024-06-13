4bd0e58d76be177fe128b3687c946f2cadd75c3c
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-06-13 15:22:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const close = document.querySelector(".close");
 const nav = document.querySelector("nav");
 
 menu.addEventListener("click", () => {
-  nav.classList.add("open-nav");
+  nav.style.width = '50%';
+  nav.style.display = 'block'
 });
 
 close.addEventListener("click", () => {
-  nav.classList.remove("open-nav");
+  nav.style.width = 0;
+  nav.style.display = 'none'
 });
