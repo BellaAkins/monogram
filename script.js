1fb7f3c05adaad31756fb7b2440ef9729fb8ccd6
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-06-13 15:31:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const nav = document.querySelector("nav");
 
 menu.addEventListener("click", () => {
   nav.style.width = "60%";
-  nav.style.display = 'block'
+  nav.style.display = "block";
 });
 
 close.addEventListener("click", () => {
   nav.style.width = 0;
-  nav.style.display = 'none'
+  nav.style.display = "none";
 });
+/*
+const shopNowBtn = document.getElementById("shopnow");
+const shoplink = document.getElementById("shoplink");
+shopNowBtn.addEventListener("click", () => {
+  shoplink.style.display = "block";
+  shopNowBtn.style.display = 0;
+});*/
